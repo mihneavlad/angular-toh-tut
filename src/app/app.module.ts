fe0a612ef0ddd2a8d18b2f2e9b00e91e1e27418e
mihneavlad <mihnel@yahoo.com>
2018-12-12 17:59:36 +0100

    Cleaned code, added an extra dashboard hero for testing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { HeroesComponent } from "./heroes/heroes.component";
 import { FormsModule } from "@angular/forms";
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import { MessagesComponent } from './messages/messages.component';
-import { AppRoutingModule } from './app-routing.module';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from "./hero-detail/hero-detail.component";
+import { MessagesComponent } from "./messages/messages.component";
+import { AppRoutingModule } from "./app-routing.module";
+import { DashboardComponent } from "./dashboard/dashboard.component";
 
 @NgModule({
-  declarations: [AppComponent, HeroesComponent, HeroDetailComponent, MessagesComponent, DashboardComponent],
+  declarations: [
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent,
+    DashboardComponent
+  ],
   imports: [BrowserModule, FormsModule, AppRoutingModule],
-  providers: [],
+
   bootstrap: [AppComponent]
 })
 export class AppModule {}
